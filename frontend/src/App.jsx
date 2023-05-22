2762e26262b7e8039593db4d245d8bb2b55955c1
927joanna <sot.joanna@gmail.com>
2023-05-22 10:08:12 +0800

    NavBar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import NavBar from "./components/NavBar";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import SongsPage from "./pages/SongsPage"
 
 function App() {
   return (
     <div>
-      <h2>GA SEI</h2>
+      <NavBar />
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      {/* main body */}
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/songs" element={<SongsPage />} />
+        </Routes>
+      </div>
     </div>
   );
 }
+
 
 export default App;
